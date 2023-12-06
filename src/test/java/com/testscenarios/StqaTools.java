a829c158ac6d168a7c0e962aab7c0b51b9d94917
Pinky Pappachan <pinkypappachan111@gmail.com>
2023-12-05 23:29:37 -0700

    new commit

---
+++ src/test/java/com/testscenarios/StqaTools.java
@@ -0,0 +1,27 @@
+package com.testscenarios;
+
+import org.testng.annotations.Test;
+
+import com.utilities.CommonFunctions;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterTest;
+
+public class StqaTools extends CommonFunctions {
+  @Test
+  public void f() {
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
